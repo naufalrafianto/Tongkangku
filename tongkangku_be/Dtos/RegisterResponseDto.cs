bebd6bc3c43caae33f6667e015a2f742dfc85845
rizalfahmii <rizalfachmii@gmail.com>
2026-08-20 03:00:37 +0700

    feat: implement user registration with role

---
+++ tongkangku_be/Dtos/RegisterResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace tongkangku_be.Dtos
+{
+    public class RegisterResponseDto
+    {
+    
+        public string name {  get; set; }
+        public string email { get; set; }
+        public string role {  get; set; }
+        public Guid id { get; set; }
+
+
+    }
+}
---
+++ tongkangku_be/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using tongkangku_be.Dtos;
+
+namespace tongkangku_be.Interfaces
+{
+    public interface IAuthService
+    {
+       
+        Task<RegisterResponseDto?> RegisterAsync(RegisterRequestDto request);
+    }
+}
