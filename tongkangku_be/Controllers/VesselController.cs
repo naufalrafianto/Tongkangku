5cf2b95fd8f4e9590c75af5792a79fb4d329ce80
rizalfahmii <rizalfachmii@gmail.com>
2026-08-23 12:54:10 +0700

    fix: exception and auth

--- tongkangku_be/Controllers/VesselController.cs
+++ tongkangku_be/Controllers/VesselController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using tongkangku_be.Dtos.RentalRequest;
 using tongkangku_be.Dtos.VesselRequest;
 using tongkangku_be.Interfaces;
 using tongkangku_be.Shared;
@@ -20,49 +19,88 @@ namespace tongkangku_be.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVesselAsync(VesselRequestDto request)
         {
-            var result = await _vesselService.CreateVesselAsync(request);
-            if (result == null)
+            try
             {
-                return BadRequest(new { message = "error", error = "gagal menambahkan vessel!" });
+                var result = await _vesselService.CreateVesselAsync(request);
+                return Ok(ApiResponse<VesselResponseDto>.SuccessResult(
+                    result,
+                    "Vessel created successfully."
+                ));
             }
-            return Ok(result);
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ApiResponse<object>.ErrorResult(ex.Message, "UNAUTHORIZED"));
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ApiResponse<object>.ErrorResult(ex.Message, "DATA_NOT_FOUND"));
+            }
+            catch (AppException ex)
+            {
+              
+                return BadRequest(ApiResponse<object>.ErrorResult(
+                    ex.Message,
+                    "BAD_REQUEST"
+                ));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, ApiResponse<object>.ErrorResult("An unexpected error occurred.", "INTERNAL_SERVER_ERROR"));
+            }
         }
 
         [HttpGet]
         public async Task<ActionResult<ApiResponse<List<VesselResponseDto>>>> GetAllVesselAsync(string? search, int limit, int page)
         {
-            var result = await _vesselService.GetAllVesselAsync(search,limit, page);
-            if (result == null || result.Count == 0)
+            try
             {
-                return NotFound(
-                    ApiResponse<List<VesselResponseDto>>.ErrorResult(
+                var result = await _vesselService.GetAllVesselAsync(search, limit, page);
+
+                if (result == null || result.Count == 0)
+                {
+                    return NotFound(ApiResponse<List<VesselResponseDto>>.ErrorResult(
                         "Vessel data not found.",
                         "VESSEL_NOT_FOUND"
-                    )
-                );
+                    ));
+                }
+
+                return Ok(ApiResponse<List<VesselResponseDto>>.SuccessResult(
+                    result,
+                    "Vessel data retrieved successfully."
+                ));
             }
-
-            return Ok(
-                   ApiResponse<List<VesselResponseDto>>.SuccessResult(
-                       result,
-                       "Vessel data retrieved successfully."
-                   )
-               );
+            catch (Exception)
+            {
+                return StatusCode(500, ApiResponse<object>.ErrorResult("An unexpected error occurred.", "INTERNAL_SERVER_ERROR"));
+            }
         }
-
 
         [HttpGet("{id:guid}")]
         public async Task<ActionResult<ApiResponse<VesselResponseDto>>> GetVesselById(Guid id)
         {
-            var result = await _vesselService.GetVesselById(id);
+            try
+            {
+                var result = await _vesselService.GetVesselById(id);
 
-            return Ok(
-                ApiResponse<VesselResponseDto>.SuccessResult(
+                return Ok(ApiResponse<VesselResponseDto>.SuccessResult(
                     result,
                     "Vessel data retrieved successfully."
-                )
-            );
+                ));
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ApiResponse<object>.ErrorResult(
+                    ex.Message,
+                    "VESSEL_NOT_FOUND"
+                ));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, ApiResponse<object>.ErrorResult(
+                    "An unexpected error occurred.",
+                    "INTERNAL_SERVER_ERROR"
+                ));
+            }
         }
-
     }
 }
